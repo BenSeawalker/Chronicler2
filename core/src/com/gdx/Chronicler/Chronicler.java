4c55b7774aa6c6709bd33deeb144ae646028afc9
BenSeawalker <benseawalker@yahoo.com>
2015-09-02 16:40:53 -0700

    Initial Commit

--- core/src/com/gdx/Chronicler/Chronicler.java
+++ core/src/com/gdx/Chronicler/Chronicler.java
@@ -1,60 +1,78 @@
 package com.gdx.Chronicler;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 //import com.badlogic.gdx.graphics.Color;
 
-public class Chronicler extends ApplicationAdapter {
-	private OrthographicCamera camera;
+public class Chronicler extends Game {
+        // CAMERA
+        private OrthographicCamera camera;
         private Viewport viewport;
+
+        // SPRITE BATCH
         SpriteBatch batch;
-        
-	Texture img;
+
+        // TEXTURES
+        Texture img;
+
+        // FONTS
         BitmapFont font;
-	
         
-	@Override
-	public void create () {
- 
-            
+        // SCREENS
+        Scene mainScreen;
+
+
+        @Override
+        public void create () {
+            // CAMERA
+            camera = new OrthographicCamera();
+            camera.setToOrtho(true, 1280, 720);
+            viewport = new FitViewport(1280, 720, camera);
+
+            // SPRITES
             batch = new SpriteBatch();
             img = new Texture("badlogic.jpg");
             
+            //FONTS
             //font = new BitmapFont(Gdx.files.internal(""), Gdx.files.internal(""), false);
             //font.setColor(Color.WHITE);
             
-	}
+            
+            // SCREENS
+           mainScreen = new Scene(this);
+           this.setScreen(mainScreen);
+        }
 
         @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-        //font.dispose();
-    }
-    
-    @Override
-    public void resize(int width, int height) {
-    }
-    
-    @Override
-    public void render () {
-            // clear the screen...
-            Gdx.gl.glClearColor(1, 0, 0, 1);
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        public void dispose() {
+            batch.dispose();
+            img.dispose();
+            font.dispose();
+        }
 
-            //Gdx.app.log(null, MD5Checksum.Get("badlogic.jpg"));
-            //Gdx.app.debug(null, );
+        @Override
+        public void resize(int width, int height) {
+            viewport.update(width, height);
+            camera.update();
+        }
 
-            // begin draw event
-            batch.begin();
-                batch.draw(img, 0, 0);
-            batch.end();
-            // end draw event
-	}
+        @Override
+        public void render () {
+            super.render();
+        }
+
+    @Override
+    public void pause() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resume() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
